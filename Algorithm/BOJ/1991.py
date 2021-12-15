61f4c4520e9c0d7d481259386e4d8cf21fc732b9
YaeheeChoe <yaehee@kakao.com>
2021-12-16 05:34:39 +0900

    Solve

--- Algorithm/BOJ/1991.py
+++ Algorithm/BOJ/1991.py
@@ -1,31 +1,30 @@
-# 트리 순회
-
-
-# 트리 배열. A : 1, ....
-MAX_N = 26
-tree= ['.','A'] + ['.'] * MAX_N*2
-
-N = int(input())
-index_dic = {}
-for i in range(1,N):
-    reaf = input().split(' ')
-    pos = ord(reaf[0])-ord('A')+1
-    tree[pos*2],tree[pos*2+1] = reaf[1],reaf[2]
-for i in range(len(tree)):
-    print(i,tree[i])
-
-def preorder(index):
-    for node in tree:
-        if node != '.':
-            print(node,end='')
-
-def postorder(index):
-    if tree[index] =='.':
+# 0: left, 1 : right
+def preorder(node):
+    if node == '.':
         return
-    postorder(index*2)
-    postorder(index*2+1)
-    print(tree[index],end='')
-    return
-preorder(1)
+    print(node,end='')
+    preorder(tree[node][0])
+    preorder(tree[node][1])
+def inorder(node):
+    if node == '.':
+        return
+    inorder(tree[node][0])
+    print(node,end='')
+    inorder(tree[node][1])
+def postorder(node):
+    if node == '.':
+        return
+    postorder(tree[node][0])
+    postorder(tree[node][1])
+    print(node,end='')
+    
+n = int(input())
+tree = {}
+for _ in range(n):
+    root,left,right = input().split()
+    tree[root] = [left,right]
+preorder('A')
 print("")
-postorder(1)
+inorder('A')
+print("")
+postorder('A')
