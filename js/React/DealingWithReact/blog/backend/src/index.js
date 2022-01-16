263c35db4d984b272391f1f0196b17cff3303faa
YaeheeChoe <yaehee@kakao.com>
2022-01-17 02:54:45 +0900

    v1

--- js/React/DealingWithReact/blog/backend/src/index.js
+++ js/React/DealingWithReact/blog/backend/src/index.js
@@ -1,12 +1,14 @@
 const Koa = require('koa')
 const Router = require('koa-router')
+const bodyParser = require('koa-bodyparser')
 const api = require('./api')
 
 const app = new Koa()
 const router = new Router()
 //라우트 모듈화
 router.use('/api', api.routes())
-
+//라우트 적용 전 바디파서 적용
+app.use(bodyParser())
 //app 인스턴스에 라우터 적용
 app.use(router.routes()).use(router.allowedMethods())
 
