09fa605c158ce06169d7fb92f01e0d7e3ddd63a1
YaeheeChoe <yaehee@kakao.com>
2022-01-17 01:45:56 +0900

    add post router

--- js/React/DealingWithReact/blog/backend/src/index.js
+++ js/React/DealingWithReact/blog/backend/src/index.js
@@ -1,24 +1,14 @@
 const Koa = require('koa')
+const Router = require('koa-router')
+const api = require('./api')
 
 const app = new Koa()
+const router = new Router()
+//라우트 모듈화
+router.use('/api', api.routes())
 
-app.use(async (ctx, next) => {
-  console.log(ctx.url)
-  console.log(1)
-  if (ctx.query.authorized !== '1') {
-    ctx.status = 401 // hrjs
-    return
-  }
-  await next()
-  console.log('END')
-})
-app.use((ctx, next) => {
-  console.log(2)
-  next()
-})
-app.use((ctx) => {
-  ctx.body = 'hello world'
-})
+//app 인스턴스에 라우터 적용
+app.use(router.routes()).use(router.allowedMethods())
 
 app.listen(4000, () => {
   console.log('Listening to port 4000')
