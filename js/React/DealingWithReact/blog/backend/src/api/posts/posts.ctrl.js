a00baa1716a66e87d113d35f0f0501ba288a51e9
YaeheeChoe <yaehee@kakao.com>
2022-01-20 16:34:51 +0900

    error 확인용 소스

--- js/React/DealingWithReact/blog/backend/src/api/posts/posts.ctrl.js
+++ js/React/DealingWithReact/blog/backend/src/api/posts/posts.ctrl.js
@@ -8,28 +8,27 @@ const posts = [
   },
 ]
 
-exports.write = (ctx) => {
-  const { title, body } = ctx.request.body
-  postId += 1
-  const post = { id: postId, title, body }
-  posts.push(post)
-  ctx.body = post
-}
 exports.list = (ctx) => {
   ctx.body = posts
 }
 exports.read = (ctx) => {
   const { id } = ctx.params
-  const index = posts.find((p) => p.id.toString() === id)
-  if (index === -1) {
+  const post = posts.find((p) => p.id.toString() === id)
+  if (!post) {
     ctx.status = 404
     ctx.body = {
       message: '포스트가 존재하지 않습니다.',
     }
     return
   }
-  posts.splice(index, 1)
-  ctx.status = 204
+  ctx.body = post
+}
+exports.write = (ctx) => {
+  const { title, body } = ctx.request.body
+  postId += 1
+  const post = { id: postId, title, body }
+  posts.push(post)
+  ctx.body = post
 }
 exports.replace = (ctx) => {
   const { id } = ctx.params
--- js/React/DealingWithReact/blog/backend/src/index.js
+++ js/React/DealingWithReact/blog/backend/src/index.js
@@ -1,8 +1,20 @@
+require('dotenv').config()
 const Koa = require('koa')
 const Router = require('koa-router')
 const bodyParser = require('koa-bodyparser')
-const api = require('./api')
+const mongoose = require('mongoose')
 
+const { PORT, MONGO_URI } = process.env
+mongoose
+  .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: false })
+  .then(() => {
+    console.log('CONNECTED MONGODB')
+  })
+  .catch((e) => {
+    console.error(e)
+  })
+
+const api = require('./api')
 const app = new Koa()
 const router = new Router()
 //라우트 모듈화
@@ -12,6 +24,7 @@ app.use(bodyParser())
 //app 인스턴스에 라우터 적용
 app.use(router.routes()).use(router.allowedMethods())
 
-app.listen(4000, () => {
-  console.log('Listening to port 4000')
+const port = PORT || 4000
+app.listen(port, () => {
+  console.log('Listening to port %d', port)
 })
