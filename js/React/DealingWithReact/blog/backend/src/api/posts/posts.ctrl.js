428d1603559e57d548b4278882cbafb3df540e9b
YaeheeChoe <yaehee@kakao.com>
2022-01-25 00:23:25 +0900

    joi 사용한 write 검증

--- js/React/DealingWithReact/blog/backend/src/api/posts/posts.ctrl.js
+++ js/React/DealingWithReact/blog/backend/src/api/posts/posts.ctrl.js
@@ -1,5 +1,6 @@
 import Post from '../../models/post'
 import mongoose from 'mongoose'
+import Joi from 'joi'
 const { ObjectId } = mongoose.Types
 
 export const checkObjectId = (ctx, next) => {
@@ -12,6 +13,17 @@ export const checkObjectId = (ctx, next) => {
 }
 
 export const write = async (ctx) => {
+  const schema = Joi.object().keys({
+    title: Joi.string().required(),
+    body: Joi.string().required(),
+    tags: Joi.array().items(Joi.string()).required(),
+  })
+  const result = schema.validate(ctx.request.body)
+  if (result.error) {
+    ctx.status = 400
+    ctx.body = result.error
+    return
+  }
   const { title, body, tags } = ctx.request.body
   const post = new Post({
     title,
@@ -57,6 +69,17 @@ export const remove = async (ctx) => {
   }
 }
 export const update = async (ctx) => {
+  const schema = Joi.object().keys({
+    title: Joi.string(),
+    body: Joi.string(),
+    tags: Joi.array().items(Joi.string()),
+  })
+  const result = schema.validate(ctx.request.body)
+  if (result.error) {
+    ctx.status = 400
+    ctx.body = result.error
+    return
+  }
   const { id } = ctx.params
   try {
     const post = await Post.findByIdAndUpdate(id.trim(), ctx.request.body, {
