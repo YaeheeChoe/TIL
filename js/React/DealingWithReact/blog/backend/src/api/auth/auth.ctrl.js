689151fc9f983fcda2e380197355f92253461b69
YaeheeChoe <yaehee@kakao.com>
2022-01-27 19:25:26 +0900

    add auth register 구현

--- js/React/DealingWithReact/blog/backend/src/api/auth/auth.ctrl.js
+++ js/React/DealingWithReact/blog/backend/src/api/auth/auth.ctrl.js
@@ -3,7 +3,37 @@ import User from '../../models/user'
 
 export const register = async (ctx) => {
   //joi 검증
+  const schema = Joi.object().keys({
+    username: Joi.string().alphanum().min(3).max(20).required(),
+    password: Joi.string().required(),
+  })
+  const result = schema.validate(ctx.request.body)
+  if (result.error) {
+    ctx.status = 400
+    ctx.body = result.error
+    return
+  }
+  const { username, password } = ctx.request.body
   // username 존재하느지 확인
+
+  try {
+    const exist = await User.findByUsername(username)
+    if (exist) {
+      ctx.status = 409
+      return
+    }
+    const user = new User({
+      username,
+    })
+    // user password설정
+    await user.setPassword(password)
+    // user.save
+    await user.save()
+    // 응답할 데이터에서 hashedPassword필드 제거
+    ctx.body = user.serialize()
+  } catch (e) {
+    ctx.throw(500, e)
+  }
 }
 export const login = async (ctx) => {}
 export const check = async (ctx) => {}
--- js/React/DealingWithReact/blog/backend/src/models/user.js
+++ js/React/DealingWithReact/blog/backend/src/models/user.js
@@ -9,9 +9,14 @@ UserSchema.methods.setPassword = async function (password) {
   const hash = await bcrypt.hash(password, 10)
   this.hashedPassword = hash
 }
-UserSchema.method.checkPassword = async function (password) {
+UserSchema.methods.checkPassword = async function (password) {
   const result = await bcrypt.compare(password, this.hashedPassword)
   return result
+}
+UserSchema.methods.serialize = function () {
+  const data = this.toJSON()
+  delete data.hashedPassword
+  return data
 }
 UserSchema.statics.findByUsername = function (username) {
   return this.findOne({ username })
