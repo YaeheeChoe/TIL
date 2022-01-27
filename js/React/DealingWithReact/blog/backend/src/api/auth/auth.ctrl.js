e0a6f0aab57e36f941c69ec8904de1077c8c729d
YaeheeChoe <yaehee@kakao.com>
2022-01-27 19:05:33 +0900

    add auth

---
+++ js/React/DealingWithReact/blog/backend/src/api/auth/auth.ctrl.js
@@ -0,0 +1,10 @@
+import Joi from 'joi'
+import User from '../../models/user'
+
+export const register = async (ctx) => {
+  //joi 검증
+  // username 존재하느지 확인
+}
+export const login = async (ctx) => {}
+export const check = async (ctx) => {}
+export const logout = async (ctx) => {}
